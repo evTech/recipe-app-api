e2b3c2b663fa7503e4a7d01904348eafd20fc9cb
Eric Vaughen <evaughen@INDUSTRIALTIRES.LOCAL>
2019-11-13 13:19:02 -0500

    user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successul(self):
+        """Test creating a new user with an email is successful """
+        email = 'test@vaughen.com'
+        password = 'TestPaswword123!'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email for a new user is normalized"""
+        email = 'test@VAUGHEN.com'
+        user = get_user_model().objects.create_user(
+                email=email,
+                password='test123'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating a user with no email error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                    email=None,
+                    password='test123'
+            )
+
+    def test_create_new_superuser(self):
+        """Test creating a new super user """
+        email = 'test@vaughen.com'
+        password = 'TestPaswword123!'
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
