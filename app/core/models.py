42dc82b4472350ed02cb645f773a25a56ec45564
Eric Vaughen <evaughen@INDUSTRIALTIRES.LOCAL>
2019-12-18 14:07:23 -0500

    Recipe object

--- app/core/models.py
+++ app/core/models.py
@@ -37,14 +37,13 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
 
-
 class Tag(models.Model):
     """Tag to be used for a recipe"""
-    name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
@@ -52,11 +51,28 @@ class Tag(models.Model):
 
 class Ingredient(models.Model):
     """Ingredient to be used for a recipe"""
-    name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
